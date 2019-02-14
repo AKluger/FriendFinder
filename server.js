a7bf36a4db1c9901d8374628ade49b445d164d99
AKluger <42947972+AKluger@users.noreply.github.com>
2019-02-13 22:59:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 // there should be logic here to take in JSON post sent from user form and store as new user
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
