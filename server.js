7d9a1375642d7b63b79487fa2df66c6a0aa870b4
kmop <42947972+AKluger@users.noreply.github.com>
2019-02-15 20:12:51 -0500

    most html done with some styling and content basic server functionality to home route

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ var path = require("path");
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+// var routes = require("./app/routing/htmlRoutes");
 // middleware for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use('/', routes);
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app)
 
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./app/routing/htmlRoutes/"));
+// });
 // Routes
 // There will be multiple html routes and api routes referenced from distinct files
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+
 // there should be logic here to take in JSON post sent from user form and store as new user
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
+
