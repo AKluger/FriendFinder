b1cebb637b5fc112b364f2c50288234a60deb56a
kmop <42947972+AKluger@users.noreply.github.com>
2019-02-17 22:35:07 -0500

    modal finally displays but blank must reference data returned

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,16 +17,17 @@ module.exports = function (app) {
     // route for POST handles incoming submission AND comparision logic to detemrine if its a match
     app.post("/api/friends", function (req, res) {
 
-
+        res.json(true);
         // our friend that matches
         var scoreMatch=50;
         var currentUser = req.body;
         for (var i = 0; i < friendsData.length; i++) {
-            if (currentUser.name !== friendsData[i].name) { friendsData.push(currentUser); 
+            if (currentUser.name !== friendsData[i].name) {  
                 var newArr = [];
             for (var j = 0; j < friendsData[i].scores.length; j++) {
-               
+            //    function to compute the difference in user scores for each question
                 var diff = function (a, b) { return Math.abs(a - b) }
+                // function to compute overall difference in scores
                 var getSum = function (total, num) {
                     return total + num;
                 }
@@ -35,19 +36,22 @@ module.exports = function (app) {
                 newArr.push(diff(friendScore, userScore))
                 
             }
-            // get total difference
             var total = newArr.reduce(getSum);
             if (total < scoreMatch)  {
                 scoreMatch = total
                 match = friendsData[i]
-
+                   while (friendsData.length === (i - 1))
+                    {           console.log(total);
+                        console.log(scoreMatch)}
             }
-            console.log(total);
+ 
         }
-        console.log(scoreMatch)
-        console.log(match)
+
     }
-        // res.json(true);}
+    console.log(scoreMatch)
+    console.log(match)
+    friendsData.push(currentUser)
+        
      
         // save match as bestMatch and send to modal ?
         // {for each score value compare to score value in res[i].scores }
